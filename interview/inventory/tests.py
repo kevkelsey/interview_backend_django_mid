896f02984f4bdfca8f4bf29d926993a103423cab
Kevin Kelsey <kevkelsey@gmail.com>
2023-05-26 10:04:15 -0700

    Revised tests & formatting

--- interview/inventory/tests.py
+++ interview/inventory/tests.py
@@ -1,19 +1,22 @@
 from django.shortcuts import reverse
-
 from rest_framework.test import APIRequestFactory, APITestCase
 
-
-from .models import Inventory
 from .views import InventoryListByCreatedAfterDate
 
-from datetime import datetime
 
 class GetInventoryCreatedAfterDate(APITestCase):
+    fixtures = ["fixtures/test_data.json"]
+
     def setUp(self):
         self.factory = APIRequestFactory()
-        self.view =  InventoryListByCreatedAfterDate.as_view()
+        self.view = InventoryListByCreatedAfterDate.as_view()
 
     def test_created_by_list(self):
-        request = self.factory.get(reverse('inventory-created-after', kwargs={'year': 2023, 'month': 5, 'day': 19}))
+        request = self.factory.get(
+            reverse(
+                "inventory-created-after", kwargs={"year": 2023, "month": 5, "day": 19}
+            )
+        )
         response = self.view(request, year=2023, month=5, day=19)
-        assert response.status_code == 200
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 17)
